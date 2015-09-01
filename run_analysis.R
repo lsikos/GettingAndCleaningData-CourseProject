98fcaf258c3125f76d54d9d71d59ae12e4349c6d
lsikos <lsikos@gmail.com>
2015-09-01 15:35:12 +0200

    Update run_analysis.R
    Revise method for extracting mean() and std()

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,21 @@
+
+############################################################################################
 # Instructions
-#-------------------------------------------------------------------------------------------
-# You should create one R script called run_analysis.R that does the following. 
-# Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
-# Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names. 
-# From the data set in step 4, creates a second, independent tidy data set with the 
-# average of each variable for each activity and each subject.
-#-------------------------------------------------------------------------------------------
+############################################################################################
+#
+# You should create one R script called run_analysis.R that does the following:
+#
+#  1. Merges the training and the test sets to create one data set.
+#  2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+#  3. Uses descriptive activity names to name the activities in the data set
+#  4. Appropriately labels the data set with descriptive variable names. 
+#  5. From the data set in step 4, creates a second, independent tidy data set with the 
+#     average of each variable for each activity and each subject.
+############################################################################################
+
+# ------------------------------------------------------------------------------------------
+# 0. Load packages and read data
+# ------------------------------------------------------------------------------------------
 
 # load packages
 library(dplyr)
@@ -39,6 +47,10 @@ for (dir in dirs) {
 subs_train  <- unique(subject_train[,1])
 subs_test  <- unique(subject_test[,1])
 
+# ------------------------------------------------------------------------------------------
+# 1. Merge the training and test sets to create one data set
+# ------------------------------------------------------------------------------------------
+
 # Unify pieces of training data set
 data_train <- cbind(y_train, X_train) # add numeric activity lables
 data_train <- cbind(subject_train, data_train) # add subject ids
@@ -47,42 +59,35 @@ data_train <- cbind(subject_train, data_train) # add subject ids
 data_test <- cbind(y_test, X_test) # add numeric activity lables
 data_test <- cbind(subject_test, data_test) # add subject ids
 
-## Merge training and test data
+# Merge training and test data
 data <- rbind(data_train, data_test)
 
 # add header
 header <- as.character(features[ ,2]) # convert features to character vector
 names(data) <- c("subject","activity", header) # add features as header to data
 
-## Extract only measurements on mean and stdev for each measurement
+# ------------------------------------------------------------------------------------------
+# 2. Extract only the measurements on the mean and standard deviation for each measurement. 
+# ------------------------------------------------------------------------------------------
 
-####  Note: header contains duplicated variable names, which causes problems for select()
-####    data <- select(data, contains("mean"))                       
-####    Error: found duplicated column name
-####  therefore need to remove those cols before using select()
-
-data  <- data[ , !duplicated(header)] # remove cols with duplicated col names
 data <- data[ , grepl("subject|activity|mean()|std()", colnames(data))]
 
+# ------------------------------------------------------------------------------------------
+# 3. Uses descriptive activity names to name the activities in the data set.
+# ------------------------------------------------------------------------------------------
 
-## Use descriptive activity names to name the activities in the data set
-
-
-
-# convert numeric activity labels to descriptive labels
 for (i in 1:length(activity_labels)) {
         data$activity[ data$activity == i] <- activity_label[i] 
 }  
-# use apply-split instead
-a <- data$activity
-l <- split(data, a)
-l <- lapply(l, )
 
-## Appropriately label data set with descriptive variable names
+# ------------------------------------------------------------------------------------------
+# 4. Appropriately label the data set with descriptive variable names.
+# ------------------------------------------------------------------------------------------
 
-## From the data set in step 4, creates a second, independent tidy data set with the 
-## average of each variable for each activity and each subject.
 
+# ------------------------------------------------------------------------------------------
+# 5. Create a data set with average of each variable for each activity and each subject.
+# ------------------------------------------------------------------------------------------
 
 
 
